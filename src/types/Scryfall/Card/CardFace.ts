14eb381b5cba8c0af5b1ea8d673718571f67d620
Federico Barbieri <barbieri.federico96@gmail.com>
2025-05-23 01:10:33 +0200

    feat(types): create CardFace and RelatedCards, remove values

---
+++ src/types/Scryfall/Card/CardFace.ts
@@ -0,0 +1,25 @@
+export type ScryfallCardFace = {
+  artist?: string
+  artist_id?: string
+  cmc?: number
+  color_indicator?: Array<string>
+  colors?: Array<string>
+  defense?: string
+  flavor_text?: string
+  illustration_id?: string
+  image_uris?: string
+  layout?: string
+  loyalty?: string
+  mana_cost: string
+  name: string
+  object: string
+  oracle_id?: string
+  oracle_text?: string
+  power?: string
+  printed_name?: string
+  printed_text?: string
+  printed_type_line?: string
+  toughness?: string
+  type_line?: string
+  watermark?: string
+}
